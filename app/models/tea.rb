69a80d48f622404eeb6fb0aa99166d3625648202
danembb <dbrophy720@gmail.com>
2022-01-12 02:49:57 -0700

    Create teas test file

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,11 @@
 class Tea < ApplicationRecord
   has_many :subscriptions
   has_many :comments, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :keywords, presence: true
+  validates :origin, presence: true
+  validates :brew_time, presence: true
+  validates :temperature, presence: true
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Tea, type: :model do
     it { should have_many(:subscriptions) }
     it { should have_many(:comments) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:keywords) }
+    it { should validate_presence_of(:origin) }
+    it { should validate_presence_of(:brew_time) }
+    it { should validate_presence_of(:temperature) }
+  end
 end
