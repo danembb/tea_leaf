aa37d585e56efadc3e71a3b7d8903afdb48760ef
danembb <dbrophy720@gmail.com>
2022-01-14 00:10:15 -0700

    Create teafacade and teaporo with corresponding testing passing

--- app/facades/tea_facade.rb
+++ app/facades/tea_facade.rb
@@ -1,9 +1,9 @@
 class TeaFacade
     class << self
         def one_tea(name)
-            response = TeaService.get_a_tea(name)
-        
-            Tea.new(response)
+            data = TeaService.get_a_tea(name)
+
+            TeaPoro.new(data)
         end
     end
 end
--- app/services/tea_service.rb
+++ app/services/tea_service.rb
@@ -1,7 +1,9 @@
 class TeaService
     class << self
         def get_a_tea(name)
-            response = BaseService.conn('https://tea-api-vic-lo.herokuapp.com/').get("/tea/#{name}")
+            response = BaseService.conn('https://tea-api-vic-lo.herokuapp.com/').get("/tea/#{name}") do |req|
+                req.params["name"]
+            end
             BaseService.parse_json(response)
         end
     end
